b3809b0889888ca6c4977c2e62e4ebc7a911b027
Emil Brunngard <brunngaard.emil@gmail.com>
2023-09-12 22:23:10 +0200

    added script to link button to new html page

---
+++ script.js
@@ -0,0 +1,11 @@
+// Make button link to new html page
+const button = document.getElementById("btn");
+
+// Add a click event listener to the button
+button.addEventListener("click", function() {
+    // Redirect to a new HTML page
+    window.location.href = "teams.html";
+});
+
+// Make button link to new html page end
+
