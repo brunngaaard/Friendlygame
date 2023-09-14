a2f354898ff6e0be1fe1bc72cbbb84163d321c75
Emil Brunngard <brunngaard.emil@gmail.com>
2023-09-14 22:05:11 +0200

    added link to header text, added form when you click on the register button. The form is buggy atm

--- script.js
+++ script.js
@@ -9,3 +9,30 @@ button.addEventListener("click", function() {
 
 // Make button link to new html page end
 
+// Get references to the button and the pop-up form
+const showFormButton = document.getElementById("showFormButton");
+const popupForm = document.getElementById("popupForm");
+const closeFormButton = document.getElementById("closeFormButton");
+
+// Function to show the pop-up form
+function showForm() {
+    popupForm.style.display = "block";
+}
+
+// Function to hide the pop-up form
+function hideForm() {
+    popupForm.style.display = "none";
+}
+
+// Event listener to show the form when the button is clicked
+showFormButton.addEventListener("click", showForm);
+
+// Event listener to hide the form when the close button is clicked
+closeFormButton.addEventListener("click", hideForm);
+
+// Event listener to hide the form when clicking outside of it
+window.addEventListener("click", function (event) {
+    if (event.target === popupForm) {
+        hideForm();
+    }
+});
