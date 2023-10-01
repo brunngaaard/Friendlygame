c8fa72ad068cf5ec1801f9dd645e1e0239292a86
Emil Brunngard <brunngaard.emil@gmail.com>
2023-10-01 12:55:47 +0200

    removed button, added pop up to cards in main

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// Make button link to new html page
-const button = document.getElementById("btn");
-
-// Add a click event listener to the button
-button.addEventListener("click", function() {
-    // Redirect to a new HTML page
-    window.location.href = "teams.html";
-});
-
-// Make button link to new html page end
 
 // Get references to the button and the popup form
 const showFormButton = document.getElementById("showFormButton");
