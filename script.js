eac0878286fca2def30dd1004b142b1f22718fe3
Emil Brunngard <brunngaard.emil@gmail.com>
2023-10-22 12:31:18 +0200

    added link to teams page, removed download page

--- script.js
+++ script.js
@@ -38,5 +38,14 @@ document.addEventListener("click", function (event) {
     popupForm.classList.add("hidden");
   });
 
+   // Get a reference to the button element
+   var button = document.getElementById("myButton");
+
+   // Add a click event listener to the button
+   button.addEventListener("click", function() {
+       // Redirect to the new HTML page
+       window.location.href = "teams.html";
+   });
+
 
   
