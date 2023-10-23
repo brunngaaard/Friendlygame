3a6486579cf1c6d26fe4b354955c1820bdac5e28
Emil Brunngard <brunngaard.emil@gmail.com>
2023-10-23 19:29:19 +0200

    removed js code

--- script.js
+++ script.js
@@ -1,64 +1,4 @@
-
-// Get references to the button and the popup form
-const showFormButton = document.getElementById("showFormButton");
-const popupForm = document.getElementById("popupForm");
-const submit = document.getElementById("submit-button")
-
-// Add a click event listener to the button
-showFormButton.addEventListener("click", function () {
-  // Toggle the "hidden" class to show/hide the form
-  if (popupForm.classList.contains("hidden")) {
-    popupForm.classList.remove("hidden");
-  } else {
-    popupForm.classList.add("hidden");
-  } 
-});
-
-
-// Function to close the form
-function closeForm() {
-  popupForm.classList.add("hidden");
-}
-
-// Add a click event listener to the document
-document.addEventListener("click", function (event) {
-    // Check if the clicked element is not inside the form or the submit button
-    if (!popupForm.contains(event.target) && event.target.tagName !== "BUTTON") {
-      closeForm(); // Close the form
-    }
-  });
-
- submit.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-
-    // Process the form data here if needed
-
-    // Close the form
-    popupForm.classList.add("hidden");
-
-  });
-
-  submit.addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-
-    // Process the form data here if needed
-
-    // Clear the form fields
-    const nameInput = document.getElementById("name"); // Replace with your input field IDs
-    const emailInput = document.getElementById("email"); // Replace with your input field IDs
-    const areaInput = document.getElementById("area"); // Replace with your input field IDs
-
-
-    nameInput.value = "";
-    emailInput.value = "";
-    areaInput.value = "";
-
-    // Close the form
-    popupForm.classList.add("hidden");
-});
-
-
-   // Get a reference to the button element
+// Get a reference to the button element
    var button = document.getElementById("myButton");
 
    // Add a click event listener to the button
@@ -66,4 +6,5 @@ document.addEventListener("click", function (event) {
        // Redirect to the new HTML page
        window.location.href = "teams.html";
    });
+
 
