a5a776266bb5f19dd8ef289181b765e7172ac76c
Emil Brunngard <brunngaard.emil@gmail.com>
2023-10-24 19:32:45 +0200

    adde a new sign up form

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Get a reference to the button element
-   var button = document.getElementById("myButton");
+  const button = document.getElementById("myButton");
 
    // Add a click event listener to the button
    button.addEventListener("click", function() {
@@ -7,4 +7,28 @@
        window.location.href = "teams.html";
    });
 
+   document.addEventListener("DOMContentLoaded", () => {
+    const openPopupButton = document.getElementById("openPopup");
+    const closePopupButton = document.getElementById("closePopup");
+    const popup = document.getElementById("popup");
 
+    openPopupButton.addEventListener("click", () => {
+        popup.style.display = "block";
+    });
+
+    closePopupButton.addEventListener("click", () => {
+        popup.style.display = "none";
+    });
+
+    // Close the popup when clicking outside the form
+    window.addEventListener("click", (e) => {
+        if (e.target === popup) {
+            popup.style.display = "none";
+        }
+    });
+
+    // Prevent the form from closing when clicking inside the popup content
+    popup.addEventListener("click", (e) => {
+        e.stopPropagation();
+    });
+});
